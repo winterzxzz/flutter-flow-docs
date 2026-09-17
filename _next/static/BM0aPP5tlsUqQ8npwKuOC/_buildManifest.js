193da6da374f5590df9bcf51b2e2e8038f6c456e
winterzxzz <athenna.2k3@gmail.com>
2026-09-17 12:50:23 +0700

    deploy: static export of docs site

---
+++ _next/static/BM0aPP5tlsUqQ8npwKuOC/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/flutter-flow-docs//_next/:path+",
+        "destination": "/flutter-flow-docs/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
